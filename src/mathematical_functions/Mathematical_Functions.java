b62bfb1046ce0675dc7f0b2172f8d6d9ed0d60e2
FabLabUSAC <fablab.usac@gmail.com>
2020-10-11 19:18:02 -0600

    Cargando el proyecto entero al repositorio

---
+++ src/mathematical_functions/Mathematical_Functions.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathematical_functions;
+
+/**
+ *
+ * @author chepo
+ */
+public class Mathematical_Functions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("ashdjfasdf");
+        Calculator calc = new Calculator();
+        calc.setVisible(true);
+    }
+    
+}
